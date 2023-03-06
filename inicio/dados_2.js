65f538aa656eb7451c3d09689bbd5c29ed145013
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-03-06 01:04:04 -0300

    atualização de  codigos

---
+++ inicio/dados_2.js
@@ -0,0 +1,20 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(preco * (1 - desconto));
+
+let precoComDesconto = preco * (1 - desconto);
+
+//Para separar as palavras, pode-se usar o espaço depois da ultima letra
+//ou pode concatenar com o simbola de + e colocar entre aspas e virgula
+let nome = "Caderno"; 
+let categoria = "Papelaria";
+
+console.log("Produto: " + nome 
++ ", Categoria: " + categoria
++ ", Preço: " + preco + ", Desconto: " + desconto 
++ ", Preço Final: " + precoComDesconto);    
+
+console.log(1+1);// Será igual a 2
+console.log("1"+1)// Se colocar um numero entre aspas e outro fora das aspas, juntara os numeros e fica 11
+
---
+++ inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 5324.56;
+console.log(typeof salario);
+
+let estaChovendo = false; // ou true
+console.log(typeof estaChovendo);
+
+console.log(typeof "teste com duas aspas");
+console.log(typeof "teste com aspas simples");
